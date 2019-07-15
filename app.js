6d0a44bccdf8ae963a7d42243b64ec23ce46522f
JoBeom-jin <whqjawls8@gmail.com>
2019-07-15 16:39:58 +0900

    static_commit

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+// app.get('/', (req, res) => res.send('Hello World!')) //
+
+
+
+app.use(express.static('public'))
+
+const path = require('path');
+const router = express.Router();
+router.get('/', function (req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+//__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+router.get('/external', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/external.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+app.use('/', router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ htmlElement_JoBeomJin/app.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => res.send('Hello World!')) //
+
+
+
+app.use(express.static('public'))
+
+const path = require('path');
+const router = express.Router();
+router.get('/', function (req, res) {
+res.sendFile(path.join(__dirname + '/index.html'));
+//__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet1', function (req, res) {
+    res.sendFile(path.join(__dirname + '/raw/sheet/stylesheet1.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+router.get('/stylesheet2', function (req, res) {
+    res.sendFile(path.join(__dirname + '/raw/sheet/stylesheet2.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+router.get('/stylesheet3', function (req, res) {
+    res.sendFile(path.join(__dirname + '/raw/sheet/stylesheet3.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+router.get('/stylesheet4', function (req, res) {
+    res.sendFile(path.join(__dirname + '/raw/sheet/stylesheet4.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+router.get('/stylesheetNo', function (req, res) {
+    res.sendFile(path.join(__dirname + '/raw/stylesheetNo.html'));
+    //__dirname : It will resolve to your project folder.
+    });
+
+
+
+
+app.use('/', router);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
